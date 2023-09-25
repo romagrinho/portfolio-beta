17a1cffe198d19b7999810e749127b5ef1078f3b
romagrinho <romariosaguilar@gmail.com>
2023-09-25 15:44:14 -0300

    iniciando

---
+++ src/Pages/Homexp/homexp.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect, useState} from "react";
+import "./homexp.css";
+
+export default function Homexp() {
+    return (
+        <section className="section1">
+            <h1>Teste</h1>
+        </section>
+    );
+}
