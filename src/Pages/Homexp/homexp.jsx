8666df2f7356b7cfa06438e0df35f5be1609c490
romagrinho <romariosaguilar@gmail.com>
2023-09-26 16:02:26 -0300

    1.2

--- src/Pages/Homexp/homexp.jsx
+++ src/Pages/Homexp/homexp.jsx
@@ -1,10 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./homexp.css";
 
 export default function Homexp() {
     return (
         <section className="section1">
-            <h1>Teste</h1>
+            <div className="containerleft">
+                <h1>Teste</h1>
+            </div>
+            <div className="containerright">
+
+            </div>
+
         </section>
     );
 }
