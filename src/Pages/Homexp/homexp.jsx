ea0dad0d016c78e8f2ee0b62ace714eed9b912ea
romagrinho <romariosaguilar@gmail.com>
2023-09-26 23:42:33 -0300

    1.3

--- src/Pages/Homexp/homexp.jsx
+++ src/Pages/Homexp/homexp.jsx
@@ -4,13 +4,15 @@ import "./homexp.css";
 export default function Homexp() {
     return (
         <section className="section1">
-            <div className="containerleft">
-                <h1>Teste</h1>
+            <div className="dadcontainer">
+                <div className="containerleft">
+                    <h1>Teste</h1>
+                </div>
+                <div className="containerright">
+                    <text className="teste">teste</text>
+                </div>
             </div>
-            <div className="containerright">
-
-            </div>
-
+            <div className="taskbar"></div>
         </section>
     );
 }
